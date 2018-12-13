5d5cf649ea5393685eeaf4f91ed2b16a04a1b1e1
Nathan Alderson <nathan.alderson@gmail.com>
2018-12-13 16:57:32 -0600

    WIP: Day 13

--- day13/src/main/scala/day13/Main.scala
+++ day13/src/main/scala/day13/Main.scala
@@ -10,29 +10,123 @@ case class Point(x:Int, y:Int)
 sealed trait Track
 object Track {
   def fromChar(c: Char): Option[Track] = {
-    if (List('|', '-').contains(c)) Some(Straight)
-    else if (List('/', '\\').contains(c)) Some(Curve)
+    if (List('|', '-', 'v', '>', '<', '^').contains(c)) Some(Straight)
+    else if (c == '/') Some(Curve('/'))
+    else if (c == '\\') Some(Curve('\\'))
     else if (c == '+') Some(Intersection)
     else None
   }
 }
 case object Straight extends Track
-case object Curve extends Track
+case class Curve(c: Char) extends Track
 case object Intersection extends Track
+
+sealed trait Direction
+object Direction {
+  def fromChar(c: Char): Option[Direction] = c match {
+    case '^' => Some(N)
+    case '>' => Some(E)
+    case 'v' => Some(S)
+    case '<' => Some(W)
+    case _ => None
+  }
+}
+case object N extends Direction
+case object E extends Direction
+case object S extends Direction
+case object W extends Direction
+
+sealed trait TurnDir
+case object L extends TurnDir
+case object R extends TurnDir
+case object Str extends TurnDir
+case class Cart(pos: Point, dir: Direction, nextTurn: TurnDir = L)
+object Cart {
+  implicit def ordering: Ordering[Cart] = Ordering.by(c => (c.pos.y, c.pos.x))
+}
 
 object Main {
   type Board = Map[Point, Track]
+  type Carts = List[Cart]
 
   def main(args: Array[String]): Unit = {
-    val input = Source.fromFile("input.txt").getLines
-    val board = parseBoard(input)
+    val input = Source.fromFile("input.txt").mkString
+    val (board, carts) = parse(input)
   }
 
-  def parseBoard(input: Iterator[String]): Board =
-    input.zipWithIndex.flatMap {
-      case (line, y) => line.zipWithIndex.map {
-        case (c, x) => Point(x, y) -> Track.fromChar(c)
-      }
-    }.toMap.filter(_._2.isDefined).mapValues(_.get)
+  // returns updated carts and a list of collision locations
+  def tick(board: Board)(cartsToMove: Carts,
+                         cartsDoneMoving: Carts = List(),
+                         collisions: List[Point] = List())
+    : (Carts, List[Point]) =
+  cartsToMove match {
+    case List() => (cartsDoneMoving, collisions)
+    case cart::rest =>
+      val (newCart, newCollision) = move(board, cartsDoneMoving++rest)(cart)
+      tick(board)(rest, (newCart::cartsDoneMoving).sorted, collisions++newCollision)
+  }
 
+  def parse(input: String): (Board, Carts) = (parseBoard(input), parseCarts(input))
+
+  def parseBoard(input: String): Board =
+    input.lines.zipWithIndex.flatMap {
+      case (line, y) => line.zipWithIndex.flatMap {
+        case (c, x) => Track.fromChar(c).map { Point(x, y) -> _ }
+      }
+    }.toMap
+
+  def parseCarts(input: String): Carts =
+    input.lines.zipWithIndex.flatMap {
+      case (line, y) => line.zipWithIndex.flatMap {
+        case (c, x) => Direction.fromChar(c).map { dir => Cart(Point(x,y), dir) }
+      }
+    }.toList.sorted
+
+  def nextTurnDir(td: TurnDir): TurnDir = td match {
+    case L => Str
+    case Str => R
+    case R => L
+  }
+
+  def curve(d: Direction, c: Curve): Direction = c match {
+    case Curve('/') => d match {
+      case N => E; case E => N; case S => W; case W => S
+    }
+    case Curve('\\') => d match {
+      case N => W; case E => S; case S => E; case W => N
+    }
+  }
+
+  def turn(d: Direction, td: TurnDir): Direction = td match {
+    case Str => d
+    case L => d match {
+      case N => W; case E => N; case S => E; case W => S
+    }
+    case R => d match {
+      case N => E; case E => S; case S => W; case W => N
+    }
+  }
+
+  // returns updated cart and an optional collision location
+  def move(board: Board, carts: Carts)(cart: Cart): (Cart, Option[Point]) = {
+    val newCart = board(cart.pos) match {
+      case Straight => Cart(dirOf(cart.pos, cart.dir), cart.dir, cart.nextTurn)
+      case c:Curve =>
+        val newDir = curve(cart.dir, c)
+        Cart(dirOf(cart.pos, newDir), newDir, cart.nextTurn)
+      case Intersection =>
+        val newDir = turn(cart.dir, cart.nextTurn)
+        Cart(dirOf(cart.pos, newDir), newDir, nextTurnDir(cart.nextTurn))
+    }
+    (newCart, collisions(newCart, carts))
+  }
+
+  def collisions(cart: Cart, carts: Carts): Option[Point] = carts.find(_.pos == cart.pos).map(_.pos)
+
+  def dirOf(p: Point, dir: Direction): Point = dir match {
+    case N => Point(p.x, p.y-1)
+    case S => Point(p.x, p.y+1)
+    case E => Point(p.x+1, p.y)
+    case W => Point(p.x-1, p.y)
+  }
 }
--- day13/src/test/scala/day13/MainSuite.scala
+++ day13/src/test/scala/day13/MainSuite.scala
@@ -9,6 +9,47 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class MainSuite extends FunSuite with Matchers {
-  test("") {
+  test("parseBoard") {
+    Main.parseBoard("/-+\n^\n|") should be (Map(Point(0,0)->Curve('/'), Point(1,0)->Straight, Point(2,0)->Intersection,
+      Point(0,1)->Straight, Point(0,2)->Straight))
   }
+
+  test("parseCarts") {
+    Main.parseCarts("/-><\n^\nv") should contain theSameElementsAs List(Cart(Point(2,0), E), Cart(Point(3,0), W),
+      Cart(Point(0,1), N), Cart(Point(0,2), S))
+  }
+
+  List(
+    (">--", (Cart(Point(2,0), E, L), None)),
+    (">->", (Cart(Point(2,0), E, L), Some(Point(2,0)))),
+    (">\\", (Cart(Point(1,1), S, L), None)),
+    (">+", (Cart(Point(1,-1), N, Str), None)),
+  ).foreach { case (input, expected) =>
+    test(s"move $input") {
+      val (board, carts) = Main.parse(input)
+      val (newCart, _) = Main.move(board, carts)(carts.head)
+      Main.move(board, carts)(newCart) should be (expected)
+    }
+  }
+
+  List(
+    (">-", (List(Cart(Point(1,0), E, L)), List())),
+    ("><", (List(Cart(Point(0,0), W, L), Cart(Point(1,0), E, L)), List(Point(1,0)))),
+    (">-<", (List(Cart(Point(1,0), W, L), Cart(Point(1,0), E, L)), List(Point(1,0)))),
+    (">>", (List(Cart(Point(1,0), E, L), Cart(Point(2,0), E, L)), List(Point(1,0)))),
+    (">->", (List(Cart(Point(1,0), E, L), Cart(Point(3,0), E, L)), List())),
+  ).foreach { case (input, (expectedCarts, expectedCollisions)) =>
+    test(s"tick $input") {
+      val (board, carts) = Main.parse(input)
+      val (newCarts, collisions) = Main.tick(board)(carts)
+      newCarts should contain theSameElementsAs expectedCarts
+      collisions should contain theSameElementsAs expectedCollisions
+    }
+  }
+
+  test(s"cart ordering") {
+    List(Point(1,1), Point(0,1), Point(1,0), Point(0,0)).map(Cart(_,N)).sorted should be (
+      List(Cart(Point(0,0),N,L), Cart(Point(1,0),N,L), Cart(Point(0,1),N,L), Cart(Point(1,1),N,L)))
+  }
+
 }
