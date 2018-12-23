68077f20092bdc9427acdb77c9a14b0e3556b91e
Nathan Alderson <nathan.alderson@gmail.com>
2018-12-22 22:49:28 -0600

    WIP day 22 part 2

--- day22/src/main/scala/day22/Main.scala
+++ day22/src/main/scala/day22/Main.scala
@@ -1,6 +1,6 @@
 package day22
 
-import scalaz.Memo
+import astar.{AStar, Engine}
 
 import scala.collection.mutable
 
@@ -11,11 +11,29 @@ case object Rocky extends RegionType
 case object Wet extends RegionType
 case object Narrow extends RegionType
 
+sealed trait Equipment
+case object Torch extends Equipment
+case object ClimbingGear extends Equipment
+case object Neither extends Equipment
+
+sealed trait Command
+case object Left extends Command
+case object Right extends Command
+case object Up extends Command
+case object Down extends Command
+case object EquipTorch extends Command
+case object EquipClimbingGear extends Command
+case object EquipNeither extends Command
+
+
 object Main {
+  type State = (Point, Equipment)
 
   def main(args: Array[String]): Unit = {
-    val depth = 3558
-    val target = Point(15,740)
+//    val depth = 3558
+//    val target = Point(15,740)
+    val depth = 510
+    val target = Point(10,10)
     val cave = for (x <- 0 to target.x; y <- 0 to target.y) yield Point(x,y)
     val erosionLevels = mutable.HashMap[Point, Long]()
     val geoIdxs = mutable.HashMap[Point, Long]()
@@ -72,11 +90,59 @@ object Main {
       println(rows.mkString("\n"))
     }
 
-//    show(cave)
-    val totalRisk = cave.map(riskLevel).sum
-    println(s"ans1 = $totalRisk")
-    // 11785 - too low
-  }
+    val engine: Engine[State, Command] = new Engine[State, Command] {
+      def valid(self: State): Boolean = self match {
+        case (point@Point(x,y), equip) =>
+          (x >= 0 && y >= 0) && (regionType(point) match {
+            case Rocky => equip == ClimbingGear || equip == Torch
+            case Wet => equip == ClimbingGear || equip == Neither
+            case Narrow => equip == Torch || equip == Neither
+          })
+      }
 
+      def bisimilar(self: State, other: State): Boolean = self == other
+
+      def hash(self: State): Int = self.hashCode
+
+      def transition(state: State, cmd: Command): State = (state, cmd) match {
+        case ((Point(x, y), e), Left)    => (Point(x-1, y), e)
+        case ((Point(x, y), e), Right)   => (Point(x+1, y), e)
+        case ((Point(x, y), e), Up)      => (Point(x, y-1), e)
+        case ((Point(x, y), e), Down)    => (Point(x, y+1), e)
+        case ((p, e), EquipClimbingGear) => (p, ClimbingGear)
+        case ((p, e), EquipTorch)        => (p, Torch)
+        case ((p, e), EquipNeither)      => (p, Neither)
+      }
+
+      def commands = List(Left, Right, Up, Down, EquipClimbingGear, EquipTorch, EquipNeither)
+
+      def heuristic(s1: State, s2: State): Int =
+        Math.abs(s1._1.x - s2._1.x) + Math.abs(s1._1.y - s2._1.y)
+
+      def cost(s1: State, s2: State): Double =
+        if (s1._2 == s2._2) 1 else 7
+
+      def distance(s1: State, s2: State): Double = cost(s1, s2) + heuristic(s1, s2)
+    }
+
+    val astarSolver = AStar[State, Command](
+      (Point(0,0), Torch),
+      (target, Torch),
+      engine
+    )
+
+    def pathLen(path: List[Command]): Int = {
+      path.map { command =>
+        if (command == EquipNeither || command == EquipTorch || command == EquipClimbingGear) 7
+        else 1
+      }.sum
+    }
+
+    println(s"ans1 = ${cave.map(riskLevel).sum}")
+    val path = astarSolver.computePath.get
+    println(s"path = $path")
+    println(s"ans2 = ${pathLen(path)}")
+
+  } // main
 }
 
